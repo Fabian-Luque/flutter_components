2bf70985c04bf42ac432a95acd0ce10434fb3e64
Kzelly <fabian.luque@globalconexus.com>
2019-11-21 19:09:39 -0300

    first commit

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'package:componentes/src/utils/icono_string_util.dart';
+import 'package:componentes/src/providers/menu_provider.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+
+    // print(menuProvider.opciones);
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: ( context, AsyncSnapshot<List<dynamic>> snapshot ){
+        
+
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+
+      },
+    );
+    
+  }
+
+  List<Widget> _listaItems( List<dynamic> data ) {
+
+
+    final List<Widget> opciones = [];
+
+    data.forEach( (opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto'] ),
+        leading: getIcon( opt['icon'] ),
+        trailing: Icon( Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {
+          
+        },
+      );
+
+      opciones..add( widgetTemp )
+              ..add( Divider());
+    });
+
+    return opciones;
+  }
+}
